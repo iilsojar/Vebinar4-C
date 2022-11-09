b3fb84701925fb73ae7a29584f29012dd05fed97
Svetlana <iilsojar@gmail.com>
2022-11-09 18:35:47 +0100

    массив из 8 элементовб заполненный нулями и единицами в случайном порядке

---
+++ ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿//напишите программу, которая выводит массив из 8 элементовб заполненный нулями и единицами в случайном порядке
+
+// int[]array = new int[8];
+
+// int[]array = new int[8];
+
+// for(int i = 0; i < array.Length; i++ )
+// {
+//     array[i] = new Random().Next(0,2); 
+//     Console.Write($"{array[i]} ");
+// }
+// Console.Write(array);
+
+//метод вывода и заполнения массива
+
+int[]array = new int[8];
+
+int[] NewArray(int[]arr)
+{
+    for(int i = 0; i < arr.Length; i++ )
+    {
+        arr[i] = new Random().Next(0,2); 
+        Console.Write($"{arr[i]} ");
+    }
+return arr;
+}
+NewArray(array);
+
+
+
+

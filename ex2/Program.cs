1608e2868e06b4a60b3a04d40fad29ad6f93fd78
Svetlana <iilsojar@gmail.com>
2022-11-09 15:24:14 +0100

    напишите программу, которая на вход принимает число и выдает количество цифр в числе с комментами

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -2,19 +2,19 @@
 
 Console.Clear();
 Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int countOfFigures = CountOfFigures(number);
+int number = Convert.ToInt32(Console.ReadLine()); //запросили число и конвертировали его
+int countOfFigures = CountOfFigures(number); // применение метода
 
 //Method
-int CountOfFigures (int num)
+int CountOfFigures (int num) //число
 {
-    int count = 0;
-    while(num > count)
+    int count = 0; //счетчик цифр
+    while(num > count) //пока 
     {
         num = num/10;
         count ++;
     }
-    return count;
+    return count; //выводим этот показатель потомк что интересует кол-во цифр а не сами цифры
 }
 Console.WriteLine($"количество цифр в числе {number} -> {countOfFigures}");
 

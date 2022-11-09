0f8d54d76ec2c8c2a007a61f426bd1275e981308
Svetlana <iilsojar@gmail.com>
2022-11-09 10:04:09 +0100

    напишите программу, которая на вход принимает число А и выдает сумму чисел от 1 до А

---
+++ ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿// напишите программу, которая на вход принимает число А и выдает сумму чисел от 1 до А
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumOfNumbers = SumOfNumbers(number);
+Console.WriteLine($"сумма чисел от 1 до {number} -> {sumOfNumbers}");
+
+int SumOfNumbers(int num)
+{
+    int sum = 0;
+    while(num > 0)
+    {
+        sum = sum + num;
+        num--;
+    }
+    return sum;
+}
+
+
+
